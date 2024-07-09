ecaebb02f07870cfabce30910e59f95048e0727c
SuzukiKjr <173776220+SuzukiKjr@users.noreply.github.com>
2024-07-09 18:08:56 +0900

    削除を追加し、編集の雛形を作成

--- main.mjs
+++ main.mjs
@@ -13,7 +13,17 @@ app.get("/", async (request, response) => {
   const posts = await prisma.post.findMany();
   const html = template.replace(
     "<!-- posts -->",
-    posts.map((post) => `<li>${escapeHTML(post.message)}</li>`).join(""),
+    posts.map((post) => `
+    <li>${escapeHTML(post.message)}</li> 
+      <form method="post" action="/delete">
+      <button type="submit">削除</button>
+      <input hidden name=deleteNumber value=${post.id}>
+      </form>
+      <form method="post" action="/edit">
+      <button type="submit">編集</button>
+      <input hidden name=editNumber value=${post.id}>
+      </form>
+      <!-- edit -->`).join(""),
   );
   response.send(html);
 });
@@ -21,6 +31,22 @@ app.get("/", async (request, response) => {
 app.post("/send", async (request, response) => {
   await prisma.post.create({
     data: { message: request.body.message },
+  });
+  response.redirect("/");
+});
+
+app.post("/delete", async (request, response) => {
+  await prisma.post.delete({
+    where: {id : parseInt(request.body.deleteNumber)},
+  });
+  response.redirect("/");
+});
+
+app.post("/edit", async (request, response) => {
+  //未完成部分
+  await prisma.post.update({
+    data: {message: request.body.message},
+    where: {id : parseInt(request.body.editNumber)},
   });
   response.redirect("/");
 });
